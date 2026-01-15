635dabc2126a25106133f3707f6a55577cc0ab66
Lyssa <92943554+lyssautida@users.noreply.github.com>
2026-01-15 04:07:22 +0000

    boolean, operators, list and its methods

---
+++ syntax.py
@@ -0,0 +1,18 @@
+my_list = [1,2,3,4,5, "rocketseat", True, False]
+
+if list[1] and list[7]:
+    print(my_list[1:7])
+else:
+    print(my_list)
+
+my_list.append(6) #add at end of list
+
+my_list.index(6) #result 8.
+
+my_list.insert(2, 10) #index a new element
+
+my_list.pop(3) #remove at index -1
+
+my_list.remove(True) #remove
+
+my_list.sort() #reorganize ascending order
